0be11a887ded841c9402f444dfc7220abae235c6
faby star <starFaby>
2021-10-06 08:33:25 -0500

    cambios

--- src/app/util/angularmaterial/angularmaterial.module.ts
+++ src/app/util/angularmaterial/angularmaterial.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 const AngularMaterialComponents = [
-  MatCardModule
+  MatCardModule,
+  MatIconModule
 ];
 
 @NgModule({
